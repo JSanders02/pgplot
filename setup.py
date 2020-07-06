5039d287ef71e001c3cfd365b70edf65ddf649d3
Jack Sanders <jacksanders404@gmail.com>
2020-07-06 14:36:18 +0100

    Made ready for PyPi

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pgplot',
+  packages = ['pgplot'],
+  version = '0.1',
+  license='MIT',
+  description = 'Graphing functionality, integrated with pygame!',
+  author = 'Jack Sanders',
+  author_email = 'jacksanders404@gmail.com',
+  url = 'https://github.com/JSanders02/pgplot',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['Graphs', 'Pygame', 'plotting'],
+  install_requires=[
+          'pygame',
+          'math',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
